66d0ab0d164e6bd2274e4dc7c2c309f26214f141
Ben Letton <benletton@gmail.com>
2022-03-13 17:28:54 +0000

    Separate the indexing and search files

--- search/search.go
+++ search/search.go
@@ -1,12 +1,8 @@
 package search
 
 import (
-	"hash"
 	"reflect"
 
-	"blainsmith.com/go/seahash"
-	"github.com/cespare/xxhash"
-	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
 )
 
@@ -63,86 +59,4 @@ func (ws *LocalSearchEngine) Search(guess Wordle) (*MatchResult, error) {
 	return &MatchResult{
 		LetterMatches: []MatchType{None, None, None, None, None},
 	}, nil
-}
-
-func NewIndexedDB(log logr.Logger, words []string, idFn IDFn) (*IndexedDB, error) {
-	log.WithName("IndexDB")
-	index := newAlphaMap()
-	reverseIndex := make(map[uint64]string, len(words))
-
-	for _, w := range words {
-		id, err := idFn(w)
-		if err != nil {
-			return nil, err
-		}
-
-		if a, ok := reverseIndex[id]; ok {
-			if a != w {
-				log.Info("possible collision between %v and %v\n", a, w)
-			}
-		}
-
-		reverseIndex[id] = w
-		for _, c := range []rune(w) {
-			index[c] = append(index[c], id)
-		}
-	}
-
-	return &IndexedDB{
-		size:         len(reverseIndex),
-		reverseIndex: reverseIndex,
-		index:        index,
-	}, nil
-}
-
-type IndexedDB struct {
-	size         int
-	reverseIndex map[uint64]string
-	index        map[rune][]uint64
-}
-
-type IDFn = func(string) (uint64, error)
-
-func NewHashingIDFn(hr func() hash.Hash64) IDFn {
-	return func(s string) (uint64, error) {
-		h := hr()
-		_, err := h.Write([]byte(s))
-		return h.Sum64(), err
-	}
-}
-
-var UseXXHashID IDFn = NewHashingIDFn(xxhash.New)
-var UseSeaHashID IDFn = NewHashingIDFn(func() hash.Hash64 {
-	var h hash.Hash64 = seahash.New()
-	return h
-})
-
-func newAlphaMap() map[rune][]uint64 {
-	return map[rune][]uint64{
-		'a': {},
-		'b': {},
-		'c': {},
-		'd': {},
-		'f': {},
-		'g': {},
-		'h': {},
-		'i': {},
-		'j': {},
-		'k': {},
-		'l': {},
-		'm': {},
-		'n': {},
-		'o': {},
-		'p': {},
-		'q': {},
-		'r': {},
-		's': {},
-		't': {},
-		'u': {},
-		'v': {},
-		'w': {},
-		'x': {},
-		'y': {},
-		'z': {},
-	}
 }
--- search/search_test.go
+++ search/search_test.go
@@ -2,11 +2,7 @@ package search
 
 import (
 	"errors"
-	"fmt"
-	"math/rand"
-	"runtime"
 	"testing"
-	"time"
 
 	"github.com/howzat/wordle/internal/logging"
 	"github.com/stretchr/testify/assert"
@@ -56,96 +52,4 @@ func TestLetterMatches(t *testing.T) {
 			}
 		})
 	}
-}
-
-func TestHashingConsistencyForIndexedWordDB(t *testing.T) {
-	t.Run("test indexing with xxHash", testIndexingWithHasher(UseXXHashID))
-	t.Run("test indexing with seaHash", testIndexingWithHasher(UseSeaHashID))
-}
-
-func testIndexingWithHasher(id IDFn) func(b *testing.T) {
-	return func(t *testing.T) {
-		log, err := logging.NewProductionLogger(t.Name())
-		require.NoError(t, err)
-
-		db, err := NewIndexedDB(*log, []string{"chunk", "latch"}, id)
-		require.NoError(t, err)
-
-		chunkID, err := id("chunk")
-		require.NoError(t, err)
-
-		latchID, err := id("latch")
-		require.NoError(t, err)
-
-		index := newAlphaMap()
-		index['a'] = []uint64{latchID}
-		index['c'] = []uint64{chunkID, latchID}
-		index['h'] = []uint64{chunkID, latchID}
-		index['u'] = []uint64{chunkID}
-		index['n'] = []uint64{chunkID}
-		index['k'] = []uint64{chunkID}
-		index['l'] = []uint64{latchID}
-		index['t'] = []uint64{latchID}
-
-		assert.Equal(t, db.index, index)
-		assert.Equal(t, db.reverseIndex[chunkID], "chunk")
-		assert.Equal(t, db.reverseIndex[latchID], "latch")
-	}
-}
-
-// XXHashIndexedWordDB-8   	1000000000	         0.5508 ns/op
-// SeaHashIndexedWordDB-8   1000000000	         0.5473 ns/op
-func BenchmarkTestHashingForIndexedWordDB(b *testing.B) {
-	b.Run("xxHash hasher", testHasher(UseXXHashID))
-	b.Run("seaHash hasher", testHasher(UseSeaHashID))
-}
-
-func testHasher(id IDFn) func(b *testing.B) {
-	return func(b *testing.B) {
-		log, err := logging.NewProductionLogger(b.Name())
-		require.NoError(b, err)
-
-		rand.Seed(time.Now().UnixNano())
-		size := 1000000
-		var words = make([]string, size)
-		var deduped = map[string]bool{}
-		for i := 0; i < size; i++ {
-			wordle := randomWordle()
-			letters := wordle.letters
-			words[i] = letters
-			deduped[letters] = true
-		}
-
-		db, err := NewIndexedDB(*log, words, id)
-		require.NoError(b, err)
-		b.Logf("index contains %v items", db.size)
-		assert.Equal(b, db.size, len(deduped))
-		PrintMemUsage()
-	}
-}
-
-var alphabet = []rune("abcdefghijklmnopqrstuvwxyz")
-
-func randomWordle() Wordle {
-	b := make([]rune, 5)
-	for i := range b {
-		b[i] = alphabet[rand.Intn(len(alphabet))]
-	}
-	return Wordle{
-		letters: string(b),
-	}
-}
-
-func PrintMemUsage() {
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
-	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
-	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
-	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
-	fmt.Printf("\tNumGC = %v\n", m.NumGC)
-}
-
-func bToMb(b uint64) uint64 {
-	return b / 1024 / 1024
 }
